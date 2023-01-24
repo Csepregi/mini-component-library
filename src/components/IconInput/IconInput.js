6be01551f72a4e7616226003a8d656b31dd4b6ee
Gabor Csepregi <gabor.csepregi@accenture.com>
2023-01-24 19:23:31 +0100

    solution

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,19 +1,83 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const STYLES = {
+  small: {
+    fontSize: 14,
+    iconSize: 16,
+    borderThickness: 1,
+    height: 24,
+  },
+  large: {
+    fontSize: 18,
+    iconSize: 24,
+    borderThickness: 2,
+    height: 36,
+  },
 };
+
+const IconInput = ({ label, icon, width = 250, size, ...delegated }) => {
+  const styles = STYLES[size];
+  if (!styles) {
+    throw new Error(`Unknown size passed to PrgressBar: ${size}`);
+  }
+  return (
+    <Wrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconWrapper style={{ "--size": styles.iconSize + "px" }}>
+        <Icon id={icon} size={16} />
+      </IconWrapper>
+
+      <TextInput
+        {...delegated}
+        style={{
+          "--width": width + "px",
+          "--height": styles.height + "px",
+          "--border-thickness": styles.borderThickness + "px",
+          "--font-size": styles.fontSize / 16 + "rem",
+        }}
+      />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.label`
+  display: block;
+  position: relative;
+  color: ${COLORS.gray700};
+  &:hover {
+    color: ${COLORS.black};
+    font-weight: 700;
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  height: var(--size);
+  margin: auto 0;
+`;
+
+const TextInput = styled.input`
+  height: var(--height);
+  width: var(--width);
+  font-size: var(--font-size);
+  border: none;
+  border-bottom: var(--border-thickness) solid ${COLORS.black};
+  padding-left: var(--height);
+  font-weight: 700;
+  outline-offset: 2px;
+  color: inherit;
+  &::placeholder {
+    color: ${COLORS.gray700};
+    font-weight: 400;
+  }
+`;
 
 export default IconInput;
--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,90 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const STYLES = {
+  small: {
+    height: 8,
+    padding: 0,
+    borderRadius: 4,
+  },
+  medium: {
+    height: 12,
+    padding: 0,
+    borderRadius: 4,
+  },
+  large: {
+    height: 16,
+    padding: 4,
+    borderRadius: 8,
+  },
+};
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = STYLES[size];
+  if (!styles) {
+    throw new Error(`Unknown size passed to PrgressBar: ${size}`);
+  }
+  return (
+    <>
+      <Title>{size}</Title>
+      <Wrapper
+        role="progressbar"
+        aria-valuemin="0"
+        aria-valuemax="100"
+        aria-labelledby="loadinglabel"
+        aria-valuenow={value}
+        style={{
+          "--padding": styles.padding + "px",
+          "--radius": styles.borderRadius + "px",
+        }}
+      >
+        <VisuallyHidden>{value}%</VisuallyHidden>
+        <BarWrapper>
+          <Bar
+            style={{ "--width": value + "%", "--height": styles.height + "px" }}
+          />
+        </BarWrapper>
+      </Wrapper>
+    </>
+  );
 };
+
+const Wrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: var(--radius);
+  padding: var(--padding);
+`;
+
+const Title = styled.span`
+  position: relative;
+
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 23px;
+
+  color: #000000;
+`;
+
+const BarWrapper = styled.div`
+  border-radius: 4px;
+  /* trim off corners when progress is near the wall */
+  overflow: hidden;
+`;
+
+const Bar = styled.div`
+  width: var(--width);
+  height: var(--height);
+  background-color: ${COLORS.primary};
+  border-radius: 4px 0 0 4px;
+  /* trim off corners when progress is near the wall */
+  overflow: hidden;
+`;
 
 export default ProgressBar;
